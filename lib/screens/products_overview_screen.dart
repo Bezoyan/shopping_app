72ec1c9914362cf0a952f4a68b2cc77f4f9c96be
Bezoyan <zoya1905bezoyan@gmail.com>
2020-04-14 10:38:54 +0400

    Filter items to favorite an All

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
+import '../providers/products.dart';
+
+enum FilterOptions {
+  Favorite,
+  All,
+}
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: <Widget>[
+          PopupMenuButton(
+              onSelected: (FilterOptions selectedValue) {
+                if (selectedValue == FilterOptions.Favorite) {
+                  productsContainer.showFavoritesOnly();
+                } else {
+                  productsContainer.showAll();
+                }
+              },
+              icon: Icon(Icons.more_vert),
+              itemBuilder: (_) => [
+                    PopupMenuItem(
+                      child: Text('Only Favorites'),
+                      value: FilterOptions.Favorite,
+                    ),
+                    PopupMenuItem(
+                      child: Text('Show All'),
+                      value: FilterOptions.All,
+                    ),
+                  ]),
+        ],
       ),
       body: ProductsGrid(),
     );
