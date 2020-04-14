55a4e30965a43e9b7d2ddd5902fff21f0e452563
Bezoyan <zoya1905bezoyan@gmail.com>
2020-04-14 17:57:35 +0400

    Add Shopping Cart data

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
-import '../providers/products.dart';
 
 enum FilterOptions {
   Favorite,
   All,
 }
 
-class ProductsOverviewScreen extends StatelessWidget {
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
-    final productsContainer = Provider.of<Products>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
         actions: <Widget>[
           PopupMenuButton(
               onSelected: (FilterOptions selectedValue) {
-                if (selectedValue == FilterOptions.Favorite) {
-                  productsContainer.showFavoritesOnly();
-                } else {
-                  productsContainer.showAll();
-                }
+                setState(() {
+                  if (selectedValue == FilterOptions.Favorite) {
+                    _showOnlyFavorites = true;
+                  } else {
+                    _showOnlyFavorites = false;
+                  }
+                });
               },
               icon: Icon(Icons.more_vert),
               itemBuilder: (_) => [
@@ -38,7 +44,7 @@ class ProductsOverviewScreen extends StatelessWidget {
                   ]),
         ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
